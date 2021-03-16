05ce3e275e27e70ed02618d9de910049e1c38238
octahate <geli961212@hotmail.com>
2021-03-16 17:20:51 +0100

    first task done

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -0,0 +1,22 @@
+package hu.nive.ujratervezes.zarovizsga.digitscounter;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class DigitsCounter {
+
+    public static final String NUMBERS = "0123456789";
+
+    public int getCountOfDigits(String s){
+        Set<Character> numbers = new TreeSet();
+        if (s == null || s.isBlank()){
+            return 0;
+        }
+        for (Character currentChar : s.toCharArray()){
+            if (NUMBERS.contains(currentChar.toString())){
+                numbers.add(currentChar);
+            }
+        }
+        return numbers.size();
+    }
+}
